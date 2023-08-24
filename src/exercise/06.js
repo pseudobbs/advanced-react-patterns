0d4c6f7e72a6a37b4dea481f97d2ff7e7611f7a0
Shawn Dobbs <pseudobbs@protonmail.com>
2023-08-24 18:50:33 -0500

    Exercise 6 with EC

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -1,90 +1,137 @@
 // Control Props
-// http://localhost:3000/isolated/exercise/06.js
+// 💯 don't warn in production
+// http://localhost:3000/isolated/final/06.extra-4.js
 
-import * as React from 'react'
-import {Switch} from '../switch'
+import * as React from 'react';
+import {useEffect, useReducer, useRef, useState} from 'react';
+import warning from 'warning';
+import {Switch} from '../switch';
 
 const callAll =
   (...fns) =>
   (...args) =>
-    fns.forEach(fn => fn?.(...args))
+    fns.forEach(fn => fn?.(...args));
 
 const actionTypes = {
   toggle: 'toggle',
-  reset: 'reset',
-}
+  reset: 'reset'
+};
 
 function toggleReducer(state, {type, initialState}) {
   switch (type) {
     case actionTypes.toggle: {
-      return {on: !state.on}
+      return {on: !state.on};
     }
     case actionTypes.reset: {
-      return initialState
+      return initialState;
     }
     default: {
-      throw new Error(`Unsupported type: ${type}`)
+      throw new Error(`Unsupported type: ${type}`);
     }
   }
+}
+
+function useControlledSwitchWarning(
+  controlPropValue,
+  controlPropName,
+  componentName
+) {
+  const isControlled = controlPropValue != null;
+  const {current: wasControlled} = useRef(isControlled);
+
+  useEffect(() => {
+    warning(
+      !(isControlled && !wasControlled),
+      `\`${componentName}\` is changing from uncontrolled to be controlled. Components should not switch from uncontrolled to controlled (or vice versa). Decide between using a controlled or uncontrolled \`${componentName}\` for the lifetime of the component. Check the \`${controlPropName}\` prop.`
+    );
+    warning(
+      !(!isControlled && wasControlled),
+      `\`${componentName}\` is changing from controlled to be uncontrolled. Components should not switch from controlled to uncontrolled (or vice versa). Decide between using a controlled or uncontrolled \`${componentName}\` for the lifetime of the component. Check the \`${controlPropName}\` prop.`
+    );
+  }, [componentName, controlPropName, isControlled, wasControlled]);
+}
+
+function useOnChangeReadOnlyWarning(
+  controlPropValue,
+  controlPropName,
+  componentName,
+  hasOnChange,
+  readOnly,
+  readOnlyProp,
+  initialValueProp,
+  onChangeProp
+) {
+  const isControlled = controlPropValue != null;
+  useEffect(() => {
+    warning(
+      !(!hasOnChange && isControlled && !readOnly),
+      `A \`${controlPropName}\` prop was provided to \`${componentName}\` without an \`${onChangeProp}\` handler. This will result in a read-only \`${controlPropName}\` value. If you want it to be mutable, use \`${initialValueProp}\`. Otherwise, set either \`${onChangeProp}\` or \`${readOnlyProp}\`.`
+    );
+  }, [
+    componentName,
+    controlPropName,
+    isControlled,
+    hasOnChange,
+    readOnly,
+    onChangeProp,
+    initialValueProp,
+    readOnlyProp
+  ]);
 }
 
 function useToggle({
   initialOn = false,
   reducer = toggleReducer,
-  // 🐨 add an `onChange` prop.
-  // 🐨 add an `on` option here
-  // 💰 you can alias it to `controlledOn` to avoid "variable shadowing."
+  onChange,
+  on: controlledOn,
+  readOnly = false
 } = {}) {
-  const {current: initialState} = React.useRef({on: initialOn})
-  const [state, dispatch] = React.useReducer(reducer, initialState)
-  // 🐨 determine whether on is controlled and assign that to `onIsControlled`
-  // 💰 `controlledOn != null`
+  const {current: initialState} = useRef({on: initialOn});
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-  // 🐨 Replace the next line with `const on = ...` which should be `controlledOn` if
-  // `onIsControlled`, otherwise, it should be `state.on`.
-  const {on} = state
+  if (process.env.NODE_ENV !== 'production') {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useControlledSwitchWarning(controlledOn, 'on', 'useToggle');
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useOnChangeReadOnlyWarning(
+      controlledOn,
+      'on',
+      'useToggle',
+      Boolean(onChange),
+      readOnly,
+      'readOnly',
+      'initialOn',
+      'onChange'
+    );
+  }
 
-  // We want to call `onChange` any time we need to make a state change, but we
-  // only want to call `dispatch` if `!onIsControlled` (otherwise we could get
-  // unnecessary renders).
-  // 🐨 To simplify things a bit, let's make a `dispatchWithOnChange` function
-  // right here. This will:
-  // 1. accept an action
-  // 2. if onIsControlled is false, call dispatch with that action
-  // 3. Then call `onChange` with our "suggested changes" and the action.
+  const onIsControlled = controlledOn != null;
+  const on = onIsControlled ? controlledOn : state.on;
 
-  // 🦉 "Suggested changes" refers to: the changes we would make if we were
-  // managing the state ourselves. This is similar to how a controlled <input />
-  // `onChange` callback works. When your handler is called, you get an event
-  // which has information about the value input that _would_ be set to if that
-  // state were managed internally.
-  // So how do we determine our suggested changes? What code do we have to
-  // calculate the changes based on the `action` we have here? That's right!
-  // The reducer! So if we pass it the current state and the action, then it
-  // should return these "suggested changes!"
-  //
-  // 💰 Sorry if Olivia the Owl is cryptic. Here's what you need to do for that onChange call:
-  // `onChange(reducer({...state, on}, action), action)`
-  // 💰 Also note that user's don't *have* to pass an `onChange` prop (it's not required)
-  // so keep that in mind when you call it! How could you avoid calling it if it's not passed?
+  function dispatchWithOnChange(action) {
+    if (!onIsControlled) {
+      dispatch(action);
+    }
+    onChange?.(reducer({...state, on}, action), action);
+  }
 
-  // make these call `dispatchWithOnChange` instead
-  const toggle = () => dispatch({type: actionTypes.toggle})
-  const reset = () => dispatch({type: actionTypes.reset, initialState})
+  const toggle = () => dispatchWithOnChange({type: actionTypes.toggle});
+  const reset = () =>
+    dispatchWithOnChange({type: actionTypes.reset, initialState});
 
   function getTogglerProps({onClick, ...props} = {}) {
     return {
       'aria-pressed': on,
       onClick: callAll(onClick, toggle),
-      ...props,
-    }
+      ...props
+    };
   }
 
   function getResetterProps({onClick, ...props} = {}) {
     return {
       onClick: callAll(onClick, reset),
-      ...props,
-    }
+      ...props
+    };
   }
 
   return {
@@ -92,36 +139,37 @@ function useToggle({
     reset,
     toggle,
     getTogglerProps,
-    getResetterProps,
-  }
+    getResetterProps
+  };
 }
 
-function Toggle({on: controlledOn, onChange, initialOn, reducer}) {
+function Toggle({on: controlledOn, onChange, readOnly, initialOn, reducer}) {
   const {on, getTogglerProps} = useToggle({
     on: controlledOn,
     onChange,
+    readOnly,
     initialOn,
-    reducer,
-  })
-  const props = getTogglerProps({on})
-  return <Switch {...props} />
+    reducer
+  });
+  const props = getTogglerProps({on});
+  return <Switch {...props} />;
 }
 
 function App() {
-  const [bothOn, setBothOn] = React.useState(false)
-  const [timesClicked, setTimesClicked] = React.useState(0)
+  const [bothOn, setBothOn] = useState(false);
+  const [timesClicked, setTimesClicked] = useState(0);
 
   function handleToggleChange(state, action) {
     if (action.type === actionTypes.toggle && timesClicked > 4) {
-      return
+      return;
     }
-    setBothOn(state.on)
-    setTimesClicked(c => c + 1)
+    setBothOn(state.on);
+    setTimesClicked(c => c + 1);
   }
 
   function handleResetClick() {
-    setBothOn(false)
-    setTimesClicked(0)
+    setBothOn(false);
+    setTimesClicked(0);
   }
 
   return (
@@ -149,14 +197,9 @@ function App() {
         />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
 // we're adding the Toggle export for tests
-export {Toggle}
-
-/*
-eslint
-  no-unused-vars: "off",
-*/
+export {Toggle};
